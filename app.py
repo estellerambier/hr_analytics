264cfe99f3c196691dacd99d17e5acf446e89078
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2023-02-03 16:05:31 +0100

    feat: init

---
+++ app.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="People x Data",
+    page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+if __name__ == "__main__":
+
+    #st.image("quotaclimat/utils/coverquotaclimat.png")
+
+    st.title("Data driven HR information")
+    st.markdown(
+        "Data tools will not replace HR, but HR who don't use data tools will be replaced by those who do."
+    )
+
+    #st.header("Available")
+    #st.markdown("Feature request")
---
+++ pages/99_Methodologie.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import streamlit as st
+
+
+st.markdown("How are these numbers computed?")
+st.sidebar.markdown("# 👩‍🔬 Methodo")
+tab1, tab2 = st.tabs(["Tracking OOO days", "Inclusivity"])
+
+
+btn = st.button("Celebrate!")
+if btn:
+    st.balloons()
